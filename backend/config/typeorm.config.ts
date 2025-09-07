e77993a3fa494673a6f3a1214780257eb37f8ddf
jhardiher <giraldojose814@gmail.com>
2025-09-07 00:00:14 -0500

    Agrego carpeta backend al repo principal

---
+++ backend/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true, // ⚠️ En producción debe estar en false
+};
