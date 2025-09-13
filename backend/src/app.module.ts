825c77ade71d9c27de03d06b0f1b9f115a4f6796
Jhardiher <giraldojose814@gmail.com>
2025-09-12 22:49:49 -0500

    refactor: actualizar configuración de TypeORM en app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,11 +5,23 @@ import { typeOrmConfig } from '../config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './modules/users/user_entity/user.entity';
 import { UsersModule } from './modules/users/users.module';
-import { Product } from './modules/products/products_entity/product.entity';
+import { Product } from './modules/products/products_entity/product_entity';
 import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Admin01@',
+      database: 'SegurityGAB',
+      entities: [Users, Product],
+      synchronize: true,
+    }),
+    ProductsModule,   
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
