7cb73a2953c74b24c58a5528bbe4ee4f887cc41b
Jhardiher <giraldojose814@gmail.com>
2025-09-12 22:03:17 -0500

    refactor: reorganizar entidades de productos y eliminar clase obsoleta

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from '../config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './modules/users/user_entity/user.entity';
+import { UsersModule } from './modules/users/users.module';
+import { Product } from './modules/products/products_entity/product.entity';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig)],
