19e92acf234a06ad45c4c48a29c034693fbdc131
Jhardiher <giraldojose814@gmail.com>
2025-09-13 08:21:29 -0500

    refactor: agregar configuración de JWT y mejorar el servicio de autenticación

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsersService } from '../users/users.service';
+import { Users } from '../users/user_entity/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+
+        private readonly usersService: UsersService,// genera y verifica tokents
+        @InjectRepository(Users)
+        private readonly userRepository: Repository<Users>,
+    ) {}
+}
