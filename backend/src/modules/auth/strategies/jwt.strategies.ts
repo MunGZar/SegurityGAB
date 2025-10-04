3e5462487c71a719274c6b55a4d57a8b6d0be11c
stivenn18 <stivenbarcenas@gmail.com>
2025-10-03 19:26:10 -0500

    Cambios desde Dev_Stive

--- backend/src/modules/auth/strategies/jwt.strategies.ts
+++ backend/src/modules/auth/strategies/jwt.strategies.ts
@@ -4,15 +4,19 @@ import { ConfigService } from '@nestjs/config';
 import { privateDecrypt } from 'crypto';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+
 @Injectable()
 export class JwtStrategies extends PassportStrategy(Strategy) {
 
     constructor(private configService: ConfigService) {
         super({
+           
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //extrae token del header
             ignoreExpiration: false, //para rechasar tokents vencidos
             secretOrKey: configService.get('JWT_SECRET'), //clave secreta para verificar firma del token
+
         })}
+
         async validate(payload: any) {
             return { 
 
